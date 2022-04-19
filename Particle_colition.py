cf28a46d952ccdaf77166df24780b2949fa1592a
Jonathan_Axel <joni50@live.com.mx>
2022-04-18 20:30:21 -0500

    El algoritmo de colisiones entra paredes funciona, la animación ahora representa correctamente los radios de las partículas.

--- Particle_colition.py
+++ Particle_colition.py
@@ -1,9 +1,8 @@
-from os import times
-from turtle import position
 import numpy as np
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 from matplotlib import cm
+
 
 #Here we define the particle class with.
 class Particle():
@@ -28,70 +27,55 @@ class Box():
         self.heigth = height
         self.width = width
 
-
-
-
-        
 #Here we create a random number of particles between 1,20 with random
 #mass, velocity and position within an interval.
-particles_number = np.random.randint(1,20)
+particles_number = 10#np.random.randint(20,100)
 box=Box()
 particles = [
     Particle(
         mass=np.random.uniform(0.1,10),
-        velocity=[np.random.uniform(-3,3),np.random.uniform(-3,3)],
-        position=[np.random.uniform(-2,2),np.random.uniform(-2,2)]
+        velocity=[np.random.uniform(-10,10),np.random.uniform(-10,10)],
+        position=[np.random.uniform(-2,2),np.random.uniform(-2,2)],
+        radius= np.random.uniform(0.5,3)
     )
     for i in range(particles_number)
 ]
 #Evolve the system # of times and save position of each particle in a list.
 
 
-def colition_detection(particles,box,times=200):
+def evolve_system(particles,box,times=2000):
     particle_number = len (particles)
     positions = [[] for i in range(particle_number)]
+    
     for _ in range(times):
         for i in range(particle_number):
             positions[i].append(particles[i].position)
             particles[i].move()
+            
+            if particles[i].position[0] < -box.width/2 or particles[i].position[0] > box.width/2:
+                setattr(particles[i],"position",np.array(positions[i][-1]))
+                setattr(particles[i],"velocity",np.array([-particles[i].velocity[0],particles[i].velocity[1] ]))
+            if particles[i].position[1] < -box.heigth/2 or particles[i].position[1] > box.heigth/2:
+                setattr(particles[i],"position",np.array(positions[i][-1]))
+                setattr(particles[i],"velocity",np.array([particles[i].velocity[0],-particles[i].velocity[1] ]))
+                
+            
     return positions
 
 
-times = 200
-positions = colition_detection(particles = particles,
+times = 1000
+positions = evolve_system(particles = particles,
                             box = box,
                             times = times)
 
 #Here we make individual list for x and y positions for each particle.
 x_position , y_position = [[] for i in range(particles_number)] , [[] for i in range(particles_number)]
+radius = []
 for i in range(particles_number):
+    radius.append(particles[i].radius)
     for j in range(times):
         x_position[i].append(positions[i][j][0])
         y_position[i].append(positions[i][j][1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -110,7 +94,7 @@ plt.plot([box.width/2,box.width/2],[-box.heigth/2,box.heigth/2],color="black")
 points = []
 color = cm.rainbow(np.linspace(0, 1, particles_number))
 for j, (col, mar) in enumerate(zip(color, ["o" for i in range(particles_number)])):
-    newpoint, = ax.plot(x_position[j][0], y_position[j][0], color=col, marker=mar)
+    newpoint, = ax.plot(x_position[j][0], y_position[j][0], color=col, marker=mar,ms = radius[j]*4)
     points.append(newpoint)
 
 def animation_frames(i):
